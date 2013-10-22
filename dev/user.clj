bb3d5e9963628ec152adc1b009a67be42639c8d1
Michael Gaare <michael@clickscape.com>
2013-10-22 01:17:33 -0400

    Updated convenience library requires in user ns

--- dev/user.clj
+++ dev/user.clj
@@ -7,7 +7,17 @@
             [korma.db :as kdb]
             [sauerworld.storage.db :as db]
             [sauerworld.storage.core :as core]
+            [sauerworld.storage.models.users :as users]
+            [sauerworld.storage.models.articles :as articles]
+            [sauerworld.storage.models.tournaments :as tournaments]
+            [immutant.messaging :as msg]
             [clojure.tools.namespace.repl :refer (refresh)]))
 
 (def db-path
   "resources/db/main")
+
+(require '[clojure.java.jdbc :as j])
+
+(defn do-db-test
+  [path]
+  (j/query (db/create-h2-spec path) ["SHOW TABLES"]))
